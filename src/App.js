ce6a1a57e8351251e2b4d704b2a28eff66a17822
prasanna <57886798+prash1909@users.noreply.github.com>
2023-03-03 20:37:03 +0530

    Todo Input and list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
 
-function App() {
+const App = () => {
+  const arr = [
+    1, 2
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Container'>
+        <div className='title'>
+          TODO
+        </div>
+        <form className='InputContainer'>
+          <input type={'text'} className='input' name='input' id='name'/>
+          <button>Go</button>
+        </form>
+
+        <ul className='Todolist'>
+          <li className='Todoitem'>
+            <p>Todo 1</p>
+            <button>edit</button>
+            <button>delete</button>
+          </li>
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
